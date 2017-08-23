8d6b3752a52e5dbc8e0db7c1589bbaec2065ad14
pemahoreau <pema.horeau@gmail.com>
2017-08-22 19:25:31 -0700

    added include file and a main

---
+++ includes/fractol.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phoreau <phoreau@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/09 14:57:35 by phoreau           #+#    #+#             */
+/*   Updated: 2017/07/18 14:35:59 by phoreau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <fcntl.h>
+# include <math.h>
+# include "../libft/libft.h"
+# include "macros.h"
+# include "../minilibx_macos/mlx.h"
+
+
+
+#endif
